2ce78612f8416b363f8ae05e0823f5ca1f06c5bc
Kathy Nguyen <kathyn262@gmail.com>
2020-11-19 18:55:19 -0800

    removed unused state

--- cines/src/MovieCard.js
+++ cines/src/MovieCard.js
@@ -3,17 +3,11 @@ import { Link } from 'react-router-dom';
 import './MovieCard.css';
 
 class MovieCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-
   render() {
+    let { title, year, imdbid } = this.props.movie;
     return (
-      <Link to={`/movies/:movie`}>
-        <li class="movie-item">Cras justo odio</li>
+      <Link to={`/movies/${imdbid}`}>
+        <li className="movie-item">{title} ({year})</li>
       </Link>
     )
   }
