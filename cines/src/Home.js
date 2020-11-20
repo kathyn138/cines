08433e827e97675e8de15ab8eed578d86714568a
Kathy Nguyen <kathyn262@gmail.com>
2020-11-19 16:57:06 -0800

    removed unnecessary class

--- cines/src/Home.js
+++ cines/src/Home.js
@@ -12,12 +12,11 @@ class Home extends React.Component {
   render() {
     return (
       <div className="row justify-content-center align-items-center home-row">
-        <div className="col-8 text-center app-content">
+        <div className="col-8 text-center">
           <h1>Welcome to Cines</h1>
           <p className="home-body">Search for movies today.</p>
         </div>
       </div>
-
     )
   }
 }
