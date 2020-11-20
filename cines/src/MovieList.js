3a84fb21e35290ad565a7ae65f69dcd24af7f2f1
Kathy Nguyen <kathyn262@gmail.com>
2020-11-19 16:58:39 -0800

    set up movielist

--- cines/src/MovieList.js
+++ cines/src/MovieList.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import SearchBar from './SearchBar';
+import MovieCard from './MovieCard';
+import './MovieList.css';
 
 class MovieList extends React.Component {
   constructor(props) {
@@ -10,7 +13,16 @@ class MovieList extends React.Component {
 
   render() {
     return (
-      "movie list"
+      <div className="row justify-content-center">
+        <div className="col-8 text-center">
+          <SearchBar />
+          <div className="card mx-auto movie-list-body">
+            <ul className="movie-list-group">
+              <MovieCard />
+            </ul>
+          </div>
+        </div>
+      </div>
     )
   }
 }
